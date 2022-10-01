b629909a01e806d82e1d451681db248637f9468d
bduranleau <b.duranleau@outlook.com>
2022-09-30 21:40:53 -0500

    feat(backend, client): add basic socket functionality

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 const dotenv = require('dotenv').config()
 const chalk = require('chalk')
+
 const port = process.env.PORT || 5000
 const node_env = (process.env.NODE_ENV || "production").toLocaleUpperCase()
 const app_name = (process.env.APP_NAME)
 
-
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+})
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
@@ -19,15 +27,44 @@ if (node_env == "DEVELOPMENT")
     })
 }
 
-app.get('/chat', (req, res) => {
-    res.json({status: "connected to server"})
+// app routes
+// GET /chat
+// app.get('/chat', (req, res) => {
+//     res.json({status: "connected to server"})
+// })
+// POST /chat
+// app.post('/chat', (req, res) => {
+//     console.log('POST REQUEST ',  req.body)
+//     res.status(200).send(req.body)
+// })
+
+// socket io functions
+// connect
+io.on('connection', (socket) => {
+    console.log('a user connected')
+        
+    // message
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg.message)
+    })
+
+    // join room
+    socket.on('join', () => {
+        console.log('a user joined')
+    })
+
+    // leave room
+    socket.on('leave', () => {
+        console.log('a user left')
+    })
+
+    // disconnect
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+    })
 })
 
-app.post('/chat', (req, res) => {
-    console.log('POST REQUEST ',  req.body)
-    res.status(200).send(req.body)
-})
-
-app.listen(port, () => {
+// please for the love of christ use `server` not `app`
+server.listen(port, () => {
     console.log("[%s] started on port:%s (%s)", chalk.cyan(app_name), chalk.green(port.toString()), chalk.red(node_env))
 })
--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import socket from './socket';
 import './styles/App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 
 function App() {
+  const [isConnected, setIsConnected ] = useState(socket.connected)
+
+  useEffect(() => {
+    socket.emit('message', {message: 'foo'})
+    socket.on('connect', () => {
+      setIsConnected(true)
+    })
+    
+    socket.on('disconnect', () => {
+      setIsConnected(false)
+    })
+
+    return () => {
+      socket.off('connect')
+      socket.off('disconnect')
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='root-container'>
+          <p>Connected: { '' + isConnected }</p>
           <Navbar />
           <Main />
         </div>
