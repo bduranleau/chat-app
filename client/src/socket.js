6a5ca0e7322140ba795ffaf5b14aee2945724c11
bduranleau <b.duranleau@outlook.com>
2022-10-01 14:45:02 -0500

    feat(client): add .env for client

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:5000'
+const URL = (process.env.REACT_APP_SERVER_ENDPOINT || 'http://localhost:5001')
 const socket = io(URL)
 
 export default socket
